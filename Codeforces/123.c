d61d80d454a31a0cabaee9393b5cb8931c07432a
Ashraful Islam <122999875+ashraful1023@users.noreply.github.com>
2023-02-20 23:27:48 +0600

    Sorting

---
+++ Codeforces/123.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    int A[3][4];
+    for(i=0;i<3;i++)
+    {
+        for(i=0;i<4;i++)
+        {
+            scanf("%d",&A[i][j]);
+        }
+    }
+    return 0;
+}
